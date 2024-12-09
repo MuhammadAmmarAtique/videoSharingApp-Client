c95c6031eb7d75eab0c40e8e2d274503feab018c
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-12-09 23:02:00 +0500

    added register reducers

--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -11,8 +11,48 @@ const userSlice = createSlice({
   },
   reducers: {
     // 1) signup reducers
+    signupRequest(state, action) {
+      state.loading = true;
+      state.isAuthenticated = false;
+      state.user = {};
+    },
+    signupSuccess(state, action) {
+      state.loading = false;
+      state.isAuthenticated = true;
+      state.user = action.payload;
+    },
+    signupFailed(state, action) {
+      state.loading = false,
+      state.isAuthenticated = false,
+      state.user = {};
+    },
     // Reducer for clearing errors
+    clearAllErrors(state, action) {
+    state.loading = false,
+    state.isAuthenticated = state.isAuthenticated,
+    state.user = state.user;
+    },
   },
 });
+
+export const register = (data) => async (dispatch) => {
+  dispatch(userSlice.actions.signupRequest());
+  try {
+    const response = await axios.post(
+      "http://localhost:7000/api/v1/users/register",
+      data,
+      {
+        withCredentials: true,
+        headers: { "Content-Type": "multipart/form-data" },
+      }
+    );
+    dispatch(userSlice.actions.signupSuccess(response.data.data));
+    toast.success(response.data.message);
+    dispatch(userSlice.actions.clearAllErrors());
+  } catch (error) {
+    dispatch(userSlice.actions.signupFailed());
+    dispatch(userSlice.actions.clearAllErrors());
+  }
+};
 
 export default userSlice.reducer;
