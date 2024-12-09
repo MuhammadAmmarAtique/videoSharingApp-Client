22cce109f3d132a371783c594288ab91d192f4c2
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-12-09 19:08:16 +0500

    added signup page ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/index";
+import { Home, Signup } from "./pages/index";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<Signup />} />
       </Routes>
     </Router>
   );
