9b3bdc7e0bd817759321338d2a76dcfdc95028e7
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-12-12 10:34:51 +0500

    added login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Home, Signup } from "./pages/index";
+import { Home, Signup, Login } from "./pages/index";
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-up" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
         <ToastContainer position="top-right"/>
     </Router>
